171f243f2bbc04c41e43a271663d9f784c04ca1e
xia-st <xia369278@gmail.com>
2015-12-21 18:43:37 +0800

    start new branch

--- src/pers/xia/jpython/parser/Parser.java
+++ src/pers/xia/jpython/parser/Parser.java
@@ -1,6 +1,20 @@
 package pers.xia.jpython.parser;
 
+import java.util.Stack;
+
+import pers.xia.jpython.grammar.DFA;
+import pers.xia.jpython.grammar.Grammar;
+import pers.xia.jpython.grammar.State;
+
 public class Parser
 {
-   
+    class StackEntry
+    {
+        DFA dfa;
+        State curState;
+    }
+
+    Stack<StackEntry> stack;
+    Grammar grammar;
+    Node node;
 }
